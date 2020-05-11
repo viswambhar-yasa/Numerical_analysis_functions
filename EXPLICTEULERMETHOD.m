38b3a85c3667619fa8ed7ec7176410a0d1148dda
viswambhar-yasa <58439259+viswambhar-yasa@users.noreply.github.com>
2020-05-11 16:32:48 +0200

    added changes to explict euler method

--- EXPLICTEULERMETHOD.m
+++ EXPLICTEULERMETHOD.m
@@ -1,19 +1,26 @@
+%Differential equation dy/dt=-(y+1)(y+3)
 clc;
 clear ;
 %initializing initial value for ode solving
 to=0;
 tend=2;
 yo=-2;
-h=0.1;
+h=0.5;
+%initializing of variable for IEM
 N=(tend-to)/h;
-y=zeros(N+1,1);
+YE=zeros(N+1,1);
+Y=zeros(N+1,1);
 t=linspace(to,tend,N+1);
-y(1)=yo;
+Y(1)=yo;
 
 %implementing implict Euler method 
 for i=1:N
-    fi=-((y(i)+1)*(y(i)+3));
-    y(i+1)=y(i)+h*fi;
+    %exact solution of the function
+    YE(i)=-3+ 2/(1 + exp(-2*t(i)));
+    fi=-((Y(i)+1)*(Y(i)+3));
+    Y(i+1)=Y(i)+h*fi;
 end
-plot(t,y);
+error=Y-YE;
+disp(error);
+plot(t,Y,t,YE);
 %Exact solution 
